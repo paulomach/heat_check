1f2c32f01d344cd6922325e4abf5b489dde47b80
Paulo Machado <paulo.machado@qpro.com.br>
2021-01-13 18:00:10 -0300

    PEP refactoring

--- sun_check.py
+++ sun_check.py
@@ -1,55 +1,58 @@
 def now():
     from datetime import datetime
-    return (datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
-def getWeather(latitude, longitude, api_key):
+def get_weather():
+    latitude = '-23.513084722626527'
+    longitude = '-47.59298163469899'
+    api_key = '55d54b1934ab95c1b15b877dfa724442'
     from DarkSkyAPI.DarkSkyAPI import DarkSkyClient
-    client = DarkSkyClient(api_key, (latitude, longitude), units='ca', exclude=['minutely', 'hourly'], lang='en')
+    client = DarkSkyClient(api_key, (latitude, longitude), units='ca',
+                           exclude=['minutely', 'hourly'], lang='en')
     current_weather = client.get_current()
     return current_weather.temperature, current_weather.cloudCover
 
 
-def processWeather(temp, cover):
-    if cover >= 0.9 or temp < 18.0:
-        cmd = 'enabletimer'
-    elif temp <= 27.0:
-        if cover < 0.5:
-            cmd = 'disabletimer'
+def process_weather(temperature, cloud_cover):
+    if (cloud_cover >= 0.9 and temperature < 28.0) or temperature < 20.0:
+        command = 'enabletimer'
+    elif temperature < 22 and cloud_cover > 25:
+        command = 'enabletimer'
+    elif temperature <= 27.0:
+        if cloud_cover < 0.5:
+            command = 'disabletimer'
         else:
-            cmd = 'enabletimer'
-    elif temp > 27.0:
-        cmd = 'disabletimer'
+            command = 'enabletimer'
+    elif temperature > 27.0:
+        command = 'disabletimer'
     else:
-        cmd = 'enabletimer'
-    return cmd
+        command = 'enabletimer'
+    return command
 
 
-def logCommand(cmd, temp, cover):
+def log_command(command, temperature, cloud_cover):
     from os import system
-    log_str = ' - Timer On  -' if cmd == 'enabletimer' else ' - Timer Off -'
-    log_str = now() + log_str + ' Temp: ' + str(temp) + 'C' + ' - Cloud Cover: ' + str(100 * cover) + '%'
-    system('echo ' + log_str + ' >> /var/log/sun_check.log')
+    log_str = ' - Timer On  -' if command == 'enabletimer' else ' - Timer Off -'
+    log_str = now() + log_str + ' Temp: ' + str(temperature) + 'C' + ' - Cloud Cover: ' + str(
+        100 * cloud_cover) + '%'
+    try:
+        system('echo ' + log_str + ' >> /var/log/sun_check.log')
+    except:
+        system('echo ' + log_str + ' >> ./sun_check.log')
+
     print(log_str)
 
 
-def processCommand(cmd, baseUrl, authKey):
+def process_command(command):
     import requests
-    headers = {'Authorization': authKey}
-    requests.get(url=baseUrl + '/json.htm?type=command&param=' + cmd + '&idx=3', headers=headers)
+    headers = {'Authorization': 'Basic cGF1bG86b3JsYW5kbzQ='}
+    requests.get(url='http://192.168.15.11:8080/json.htm?type=command&param=' + command + '&idx=3',
+                 headers=headers)
 
 
 if __name__ == '__main__':
-    import json
-
-    with open('config.json', 'r') as config:
-        data = json.load(config)
-
-    temp, cover = getWeather(latitude=data.get('weather').get('lat'),
-                             longitude=data.get('weather').get('long'),
-                             api_key=data.get('weather').get('key'))
-    cmd = processWeather(temp, cover)
-    processCommand(cmd=cmd,
-                   baseUrl=data.get('controller').get('baseUrl'),
-                   authKey=data.get('controller').get('authKey'))
-    logCommand(cmd, temp, cover)
+    temp, cover = get_weather()
+    cmd = process_weather(temp, cover)
+    process_command(cmd)
+    log_command(cmd, temp, cover)
